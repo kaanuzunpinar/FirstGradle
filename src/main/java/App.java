54ae855b39ff1258014a2cd54e3977d48d2a3747
kaanuzunpinar <uzunpinarkaan@gmail.com>
2021-03-13 14:29:07 +0300

    port bug fixing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,8 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        int port=Integer.parseInt(System.getenv("PORT"));
+        port(port);
        /* get("/",
                 (rq,rs)->"Hello World!");
         post("/compute", (req, res) -> {
@@ -49,8 +51,7 @@ public class App {
         post("/compute", (req, res) -> {
             //System.out.println(req.queryParams("input1"));
             //System.out.println(req.queryParams("input2"));
-            int port=Integer.parseInt(System.getenv("PORT"));
-            port(port);
+
             String input1 = req.queryParams("input1");
             java.util.Scanner sc1 = new java.util.Scanner(input1);
             sc1.useDelimiter("[;\r\n]+");
