c463f25905493bbd4a1ae10ab9fc18ce67884616
kaanuzunpinar <uzunpinarkaan@gmail.com>
2021-03-13 15:39:56 +0300

    Added 5 tests and made instructions more understandable

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,8 +12,9 @@ public class App {
     public static void main(String[] args) {
         int port=Integer.parseInt(System.getenv("PORT"));
         port(port);
-       /* get("/",
-                (rq,rs)->"Hello World!");
+
+        get("/", (req, res) -> "Hello, World. For my computation please add /compute to your link");
+
         post("/compute", (req, res) -> {
 
             String input1 = req.queryParams("input1");
@@ -29,67 +30,45 @@ public class App {
             System.out.println(inputList);
 
 
-            String input2 = req.queryParams("input2").replaceAll("\\s","");
-            int input2AsInt = Integer.parseInt(input2);
-
-            boolean result = App.search(inputList, input2AsInt);
-
-            Map<String, Boolean> map = new HashMap<String, Boolean>();
-            map.put("result", result);
-            return new ModelAndView(map, "templates/compute.mustache");
-        }, new MustacheTemplateEngine());
-
-
-        get("/compute",
-                (rq,rs)->{
-                    Map<String,String> map=new HashMap<>();
-                    map.put("result","not computed yet!");
-                    return new ModelAndView(map, "templates/compute.mustache");
-                },new MustacheTemplateEngine());*/
-        get("/", (req, res) -> "Hello, World");
-
-        post("/compute", (req, res) -> {
-            //System.out.println(req.queryParams("input1"));
-            //System.out.println(req.queryParams("input2"));
-
-            String input1 = req.queryParams("input1");
-            java.util.Scanner sc1 = new java.util.Scanner(input1);
-            sc1.useDelimiter("[;\r\n]+");
-            java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
-            while (sc1.hasNext())
+            String input2 = req.queryParams("input2");
+            java.util.Scanner sc2 = new java.util.Scanner(input2);
+            sc2.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> inputList2= new java.util.ArrayList<>();
+            while (sc2.hasNext())
             {
-                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
-                inputList.add(value);
+                int value = Integer.parseInt(sc2.next().replaceAll("\\s",""));
+                inputList2.add(value);
             }
-            sc1.close();
-            System.out.println(inputList);
+            sc2.close();
+            int a0=inputList2.get(0);
+            int a1=inputList2.get(1);
+            int a2=inputList2.get(2);
 
-
-            String input2 = req.queryParams("input2").replaceAll("\\s","");
-            int input2AsInt = Integer.parseInt(input2);
-
-            boolean result = App.search(inputList, input2AsInt);
-
-            Map<String, Boolean> map = new HashMap<String, Boolean>();
-            map.put("result", result);
+           // boolean result = App.search(inputList, input2AsInt);
+            int result=App.howManyNumbersOverAverage(inputList,a0,a1,a2);
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("result","Mean is: "+(a0+a1+a2)/3 +" There are "+result+" numbers bigger than mean.");
+            System.out.println(inputList2);
             return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
 
         get("/compute",
                 (rq, rs) -> {
                     Map<String, String> map = new HashMap<String, String>();
-                    map.put("result", "not computed yet!");
+                    map.put("result", "Please enter your numbers left bar. Enter 3 numbers to right bar. It will compute mean of 3 numbers and prints" +
+                            " how many number is bigger than mean");
                     return new ModelAndView(map, "compute.mustache");
                 },new MustacheTemplateEngine());
     }
-
-    public static boolean search(ArrayList<Integer> list,int a){
+    public static Integer howManyNumbersOverAverage(ArrayList<Integer> list,int a1,int a2,int a3){
+        int average=(a1+a2+a3)/3;
+        int don=0;
         if(list==null)
-            return false;
-        for(int temp:list)
-            if(temp==a) {
-                return true;
-            }
-        return false;
+            return null;
+        for(int a:list){
+            if(a>average)
+                don++;
+        }
+        return don;
     }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -7,9 +7,51 @@ import static org.junit.Assert.*;
 public class AppTest {
     @Test void testFound(){
         ArrayList<Integer> a1=new ArrayList<>();
-        a1.add(3);
-        a1.add(4);
-        a1.add(5);
-        assertTrue(App.search(a1,3));
+        a1.add(-1);
+        a1.add(0);
+        a1.add(-2);
+        assertTrue(App.howManyNumbersOverAverage(a1,0,0,0)==0);
+
+    }
+    @Test void testFound2(){
+        ArrayList<Integer> a1=new ArrayList<>();
+        a1.add(500);
+        a1.add(500);
+        a1.add(500);
+        a1.add(-500);
+        a1.add(200);
+        assertTrue(App.howManyNumbersOverAverage(a1,150,200,250)==3);
+
+    }
+    @Test void testFound3(){
+        ArrayList<Integer> a1=new ArrayList<>();
+        a1.add(0);
+        a1.add(0);
+        a1.add(0);
+        for(int i=-100;i<=100;i++){
+            for(int j=-100;j<=100;j++){
+                for (int k=-100;k<=100;k++){
+                    assertTrue(App.howManyNumbersOverAverage(a1,i,j,k)>=0);
+                }
+            }
+        }
+    }
+    @Test void testNull(){
+        ArrayList<Integer> a1=null;
+        assertNull(App.howManyNumbersOverAverage(a1,0,0,0));
+
+    }
+    @Test void testNotFound(){
+        ArrayList<Integer> a1=new ArrayList<>();
+        a1.add(0);
+        a1.add(0);
+        a1.add(0);
+        for(int i=-100;i<=100;i++){
+            for(int j=-100;j<=100;j++){
+                for (int k=-100;k<=100;k++){
+                    assertFalse(App.howManyNumbersOverAverage(a1,i,j,k)>3);
+                }
+            }
+        }
     }
 }
