481a69b65cb247f2bf8d99a9c206d01941aa4b4c
kaanuzunpinar <uzunpinarkaan@gmail.com>
2021-03-13 14:01:26 +0300

    Adding procfile for heroku,and settings for html

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar FirstGradle-1.0-SNAPSHOT-all.jar
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,86 @@
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
+       /* get("/",
+                (rq,rs)->"Hello World!");
+        post("/compute", (req, res) -> {
 
+            String input1 = req.queryParams("input1");
+            java.util.Scanner sc1 = new java.util.Scanner(input1);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+                inputList.add(value);
+            }
+            sc1.close();
+            System.out.println(inputList);
+
+
+            String input2 = req.queryParams("input2").replaceAll("\\s","");
+            int input2AsInt = Integer.parseInt(input2);
+
+            boolean result = App.search(inputList, input2AsInt);
+
+            Map<String, Boolean> map = new HashMap<String, Boolean>();
+            map.put("result", result);
+            return new ModelAndView(map, "templates/compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+                (rq,rs)->{
+                    Map<String,String> map=new HashMap<>();
+                    map.put("result","not computed yet!");
+                    return new ModelAndView(map, "templates/compute.mustache");
+                },new MustacheTemplateEngine());*/
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+            //System.out.println(req.queryParams("input1"));
+            //System.out.println(req.queryParams("input2"));
+
+            String input1 = req.queryParams("input1");
+            java.util.Scanner sc1 = new java.util.Scanner(input1);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+                inputList.add(value);
+            }
+            sc1.close();
+            System.out.println(inputList);
+
+
+            String input2 = req.queryParams("input2").replaceAll("\\s","");
+            int input2AsInt = Integer.parseInt(input2);
+
+            boolean result = App.search(inputList, input2AsInt);
+
+            Map<String, Boolean> map = new HashMap<String, Boolean>();
+            map.put("result", result);
+            return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+        get("/compute",
+                (rq, rs) -> {
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put("result", "not computed yet!");
+                    return new ModelAndView(map, "compute.mustache");
+                },new MustacheTemplateEngine());
     }
+
     public static boolean search(ArrayList<Integer> list,int a){
         if(list==null)
             return false;
